0bbba6a0939a5588da5df90861d06b88d6702a2c
Shaun Smiley <senorsmile@gmail.com>
2018-01-10 09:52:15 -0800

    use pipenv instead of manual junk

--- run_app.sh
+++ run_app.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-[[ -d ./venv/ ]] || {
-  virtualenv venv/
-  source ./venv/bin/activate
-  pip install -U setuptools
-  pip install -U pip
-  pip install -r requirements.txt
-  deactivate
+#[[ -d ./venv/ ]] || {
+#  virtualenv venv/
+#  source ./venv/bin/activate
+#  pip install -U setuptools
+#  pip install -U pip
+#  pip install -r requirements.txt
+#  deactivate
+#}
+#
+#
+#source ./venv/bin/activate
+#pip install -r requirements.txt
+
+# pipenv
+which pipenv || {
+  echo "Must pip install pipenv in order to run this.  Exiting."
+  exit 1
 }
 
-
-source ./venv/bin/activate
-pip install -r requirements.txt
 
 #python app.py
 export FLASK_APP=app.py
